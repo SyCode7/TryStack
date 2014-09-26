54e9e0b184ecf9a156f1bb714d7a04e0f311cf18
SyCode7 <ktorkura@yahoo.com>
2014-09-26 10:19:01 +0200

    still unsuccessful

--- TryStack2/src/main/java/org/jclouds/openstack/swift/v1/JCloudsSwift.java
+++ TryStack2/src/main/java/org/jclouds/openstack/swift/v1/JCloudsSwift.java
@@ -1,38 +1,37 @@
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.io.Closeables;
-import com.google.inject.Module;
+package org.jclouds.openstack.swift.v1;
+
+import static com.google.common.io.ByteSource.wrap;
+import static org.jclouds.io.Payloads.newByteSourcePayload;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Set;
 
 import org.jclouds.ContextBuilder;
 import org.jclouds.io.Payload;
 import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
-import org.jclouds.openstack.swift.v1.SwiftApi;
 import org.jclouds.openstack.swift.v1.domain.Container;
 import org.jclouds.openstack.swift.v1.features.ContainerApi;
 import org.jclouds.openstack.swift.v1.features.ObjectApi;
 import org.jclouds.openstack.swift.v1.options.CreateContainerOptions;
 import org.jclouds.openstack.swift.v1.options.PutOptions;
 
-import java.io.Closeable;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Set;
-
-import static com.google.common.io.ByteSource.wrap;
-import static org.jclouds.io.Payloads.newByteSourcePayload;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.Closeables;
+import com.google.inject.Module;
 
 public class JCloudsSwift implements Closeable {
    public static final String CONTAINER_NAME = "SOMETHING_FUNNY2";
    public static final String OBJECT_NAME = "premier.txt";
 
    private SwiftApi swiftApi;
-
    public static void main(String[] args) throws IOException {
       JCloudsSwift jcloudsSwift = new JCloudsSwift();
 
       try {
-         jcloudsSwift.createContainer();
-         jcloudsSwift.uploadObjectFromString();
+         jcloudsSwift.TemporaryUrlKeyFromAccount();
+    	 jcloudsSwift.uploadObjectFromString();
          jcloudsSwift.listContainers();
          jcloudsSwift.close();
       } catch (Exception e) {
@@ -42,7 +41,15 @@ public class JCloudsSwift implements Closeable {
       }
    }
 
-   public JCloudsSwift() {
+   
+   TemporaryURLSigner signedURL = new TemporaryURLSigner(null);
+   
+   private void TemporaryUrlKeyFromAccount() {
+	// TODO Auto-generated method stub
+	
+}
+
+public JCloudsSwift() {
       Iterable<Module> modules = ImmutableSet.<Module>of(
             new SLF4JLoggingModule());
 
@@ -75,11 +82,8 @@ public class JCloudsSwift implements Closeable {
       System.out.println("Upload Object From String");
 
       ObjectApi objectApi = swiftApi.getObjectApiForRegionAndContainer("RegionOne", CONTAINER_NAME);
-      
-      
-
-      
-      Payload payload = newByteSourcePayload(wrap("Hello Z Xz xZ xX ZXWorld".getBytes()));
+  
+     Payload payload = newByteSourcePayload(wrap("Hello Z Xz xZ xX ZXWorld".getBytes()));
 
       objectApi.put(OBJECT_NAME, payload, PutOptions.Builder.metadata(ImmutableMap.of("key1", "value1")));
 
---
+++ TryStack2/src/main/java/org/jclouds/openstack/swift/v1/TemporaryURLSigner.java
@@ -0,0 +1,72 @@
+package org.jclouds.openstack.swift.v1;
+
+import static com.google.common.base.Charsets.UTF_8;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Suppliers.memoizeWithExpiration;
+import static com.google.common.base.Throwables.propagate;
+import static com.google.common.io.BaseEncoding.base16;
+import static java.lang.String.format;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.jclouds.openstack.swift.v1.features.AccountApi;
+
+import com.google.common.base.Supplier;
+
+/**
+ * Use this utility to create temporary urls.
+ */
+public class TemporaryURLSigner {
+
+   public static TemporaryURLSigner checkApiEvery(final AccountApi api, long seconds) {
+      Supplier<String> keySupplier = memoizeWithExpiration(new TemporaryUrlKeyFromAccount(api), seconds, SECONDS);
+      return new TemporaryURLSigner(keySupplier);
+   }
+
+   private final Supplier<String> keySupplier;
+
+   TemporaryURLSigner(Supplier<String> keySupplier) {
+      this.keySupplier = keySupplier;
+   }
+
+   public String sign(String method, String path, long expirationTimestampSeconds) {
+      checkNotNull(method, "method");
+      checkNotNull(path, "path");
+      checkArgument(expirationTimestampSeconds > 0, "expirationTimestamp must be a unix epoch timestamp");
+      String hmacBody = format("%s\n%s\n%s", method, expirationTimestampSeconds, path);
+      return base16().lowerCase().encode(hmacSHA1(hmacBody));
+   }
+
+   byte[] hmacSHA1(String data) {
+      try {
+         String key = keySupplier.get();
+         checkState(key != null, "%s returned a null temporaryUrlKey!", keySupplier);
+         Mac mac = Mac.getInstance("HmacSHA1");
+         mac.init(new SecretKeySpec(key.getBytes(UTF_8), "HmacSHA1"));
+         return mac.doFinal(data.getBytes(UTF_8));
+      } catch (Exception e) {
+         throw propagate(e);
+      }
+   }
+
+   static class TemporaryUrlKeyFromAccount implements Supplier<String> {
+      private final AccountApi api;
+
+      private TemporaryUrlKeyFromAccount(AccountApi api) {
+         this.api = checkNotNull(api, "accountApi");
+      }
+
+      public String get() {
+         return api.get().getTemporaryUrlKey().orNull();
+      }
+
+      @Override
+      public String toString() {
+         return format("get().getTemporaryUrlKey() using %s", api);
+      }
+   }
+}
